54fce74c22f98b83c3e86d43a4cfefd7955ab185
Praneeth Reddicherla <reddicherlapraneeth@gmail.com>
2025-10-16 10:54:35 -0500

    Add Two Sum solution in two_sum.py
    Implement a solution for the Two Sum problem.

---
+++ arrays/two_sum.py
@@ -0,0 +1,14 @@
+# Problem: Two Sum
+# Difficulty: Easy
+
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        hashmap = {}
+        for i, n in enumerate(nums):
+            if target - n in hashmap:
+                return [hashmap[target - n], i]
+            hashmap[n] = i
+
+# Example:
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
